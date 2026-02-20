00a9a2924ca7547e8c3166b2aaeba774416e9cb1
LeoA5 <leoas5@protonmail.com>
2026-02-20 01:25:47 -0800

    updated secret rotation scripts

--- rotatesecrets.sh
+++ rotatesecrets.sh
@@ -1,24 +1,21 @@
-# write a new password, restart maria, and update the root user
+# Rotate all user passwords in the MariaDB from the host machine
 set -e
 
-# Needed to run the script docker commans
+# Load current root@localhost password to use for operations
 root_local_pw=$(cat ./.secrets/mariadb/rootpw.txt)
-echo $root_local_pw
 
-# Update % root
+# Update root@%
 new_wild_root_pw=$(openssl rand -base64 24 | tee ./.secrets/mariadb/root%pw.txt)
-echo $new_wild_root_pw
-echo $root_local_pw
 docker exec mariadb_main sh -c "
     mariadb --socket=/run/mysqld/mysqld.sock -u root -p\"$root_local_pw\" -e \"
         ALTER USER 'root'@'%' IDENTIFIED BY '$new_wild_root_pw';
     \"
 "
 
-# Update local root ALWAYS MAKE THIS THE LAST ONE UPDATED
+# Update root@localhost ALWAYS MAKE THIS THE LAST ONE UPDATED
+# SERIOUSLY, THIS PASSWORD IS USED TO AUTHENTICATE COMMANDS FROM THIS SHELL
+# YOU HAVE TO UPDATE ROOT_LOCAL_PW IF YOU TRY TO DO ANYTHING AFTER THIS
 new_local_root_pw=$(openssl rand -base64 24 | tee ./.secrets/mariadb/rootpw.txt)
-echo $new_local_root_pw
-echo $root_local_pw
 docker exec mariadb_main sh -c "
     mariadb --socket=/run/mysqld/mysqld.sock -u root -p\"$root_local_pw\" -e \"
         ALTER USER 'root'@'localhost' IDENTIFIED BY '$new_local_root_pw';
